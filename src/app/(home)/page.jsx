cdfb19ec46b83be4bec04e46cbf39c02616e4fef
Ellise Marimon <ellisemarimon991q@gmail.com>
2025-07-19 15:54:21 +0800

    test on homepage

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -3,5 +3,6 @@ import { HomeView } from 'src/sections/home/view';
 // ----------------------------------------------------------------------
 
 export default function Page() {
-  return <HomeView />;
+  // return <HomeView />;
+  return <h1 style={{ padding: 40 }}>✅ HOME PAGE LOADED</h1>;
 }

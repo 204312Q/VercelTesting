6305a96a30b5c7ed98cd318b79e3bff89a7120c2
Ellise Marimon <ellisemarimon991q@gmail.com>
2025-07-21 11:05:43 +0800

    Debug:  Isolate the <PostListHomeView/>

--- src/app/post/page.jsx
+++ src/app/post/page.jsx
@@ -1,22 +1,33 @@
+// import { CONFIG } from 'src/global-config';
+// import { getPosts } from 'src/actions/blog-ssr';
+
+// import { PostListHomeView } from 'src/sections/blog/view';
+
+// // ----------------------------------------------------------------------
+
+// export const metadata = { title: `Post list - ${CONFIG.appName}` };
+
+// export default async function Page() {
+//   // const { posts } = await getPosts();
+
+//   return <h1>Post List</h1>;
+
+//   // try {
+//   //   const { posts } = await getPosts();
+//   //   return <PostListHomeView posts={posts} />;
+//   // } catch (error) {
+//   //   console.error('Error fetching posts:', error);
+//   //   return <div>Error loading posts</div>; // Don't throw or return error object
+//   // }
+// }
+
 import { CONFIG } from 'src/global-config';
-import { getPosts } from 'src/actions/blog-ssr';
-
+// import { getPosts } from 'src/actions/blog-ssr';
 import { PostListHomeView } from 'src/sections/blog/view';
-
-// ----------------------------------------------------------------------
 
 export const metadata = { title: `Post list - ${CONFIG.appName}` };
 
-export default async function Page() {
-  // const { posts } = await getPosts();
-
-  return <h1>Post List</h1>;
-
-  // try {
-  //   const { posts } = await getPosts();
-  //   return <PostListHomeView posts={posts} />;
-  // } catch (error) {
-  //   console.error('Error fetching posts:', error);
-  //   return <div>Error loading posts</div>; // Don't throw or return error object
-  // }
+export default function Page() {
+  return <PostListHomeView posts={[]} />;
 }
+

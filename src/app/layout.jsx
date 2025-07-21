9959383e46780aefd1e16fd1759e6a0a74a4be91
Ellise Marimon <ellisemarimon991q@gmail.com>
2025-07-21 08:46:28 +0800

    debug: added client-layout
    for client components to render properly on server component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,39 @@
 import 'src/global.css';
 
+import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+
 import { CONFIG } from 'src/global-config';
 import { primary } from 'src/theme/core/palette';
-import ClientLayout from 'src/components/layouts/ClientLayout';
+import { LocalizationProvider } from 'src/locales';
 import { detectLanguage } from 'src/locales/server';
+import { themeConfig, ThemeProvider } from 'src/theme';
+import { I18nProvider } from 'src/locales/i18n-provider';
+
+import { Snackbar } from 'src/components/snackbar';
+import { ProgressBar } from 'src/components/progress-bar';
+import { MotionLazy } from 'src/components/animate/motion-lazy';
 import { detectSettings } from 'src/components/settings/server';
-import { defaultSettings } from 'src/components/settings';
+import { SettingsDrawer, defaultSettings, SettingsProvider } from 'src/components/settings';
+import ClientLayout from 'src/components/layout/client-layout';
+
+
+import { CheckoutProvider } from 'src/sections/checkout/context';
+
+import { AuthProvider as JwtAuthProvider } from 'src/auth/context/jwt';
+import { AuthProvider as Auth0AuthProvider } from 'src/auth/context/auth0';
+import { AuthProvider as AmplifyAuthProvider } from 'src/auth/context/amplify';
+import { AuthProvider as SupabaseAuthProvider } from 'src/auth/context/supabase';
+import { AuthProvider as FirebaseAuthProvider } from 'src/auth/context/firebase';
+
+// ----------------------------------------------------------------------
+
+const AuthProvider =
+  (CONFIG.auth.method === 'amplify' && AmplifyAuthProvider) ||
+  (CONFIG.auth.method === 'firebase' && FirebaseAuthProvider) ||
+  (CONFIG.auth.method === 'supabase' && SupabaseAuthProvider) ||
+  (CONFIG.auth.method === 'auth0' && Auth0AuthProvider) ||
+  JwtAuthProvider;
 
 // ----------------------------------------------------------------------
 
@@ -14,6 +42,8 @@ export const viewport = {
   initialScale: 1,
   themeColor: primary.main,
 };
+
+// ----------------------------------------------------------------------
 
 export const metadata = {
   title: 'Chilli Padi Confinement',
@@ -45,13 +75,41 @@ async function getAppConfig() {
   }
 }
 
+// ----------------------------------------------------------------------
+
 export default async function RootLayout({ children }) {
   const appConfig = await getAppConfig();
 
   return (
     <html lang={appConfig.lang} dir={appConfig.dir} suppressHydrationWarning>
       <body>
-        <ClientLayout appConfig={appConfig}>{children}</ClientLayout>
+        <InitColorSchemeScript
+          defaultMode={themeConfig.defaultMode}
+          modeStorageKey={themeConfig.modeStorageKey}
+          attribute={themeConfig.cssVariables.colorSchemeSelector}
+        />
+
+        <I18nProvider lang={appConfig.i18nLang}>
+          <AuthProvider>
+            <SettingsProvider
+              cookieSettings={appConfig.cookieSettings}
+              defaultSettings={defaultSettings}
+            >
+              <LocalizationProvider>
+                <AppRouterCacheProvider options={{ key: 'css' }}>
+                  <ThemeProvider
+                    defaultMode={themeConfig.defaultMode}
+                    modeStorageKey={themeConfig.modeStorageKey}
+                  >
+                    <MotionLazy>
+                      <ClientLayout>{children}</ClientLayout>
+                    </MotionLazy>
+                  </ThemeProvider>
+                </AppRouterCacheProvider>
+              </LocalizationProvider>
+            </SettingsProvider>
+          </AuthProvider>
+        </I18nProvider>
       </body>
     </html>
   );

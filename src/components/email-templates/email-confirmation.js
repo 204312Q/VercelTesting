989bfc1b828767fd555a26a49b4b2b38ae399b65
Ellise Marimon <94149292+204312Q@users.noreply.github.com>
2025-11-07 16:46:01 +0800

    Updated the riceOption display
    instead of display "WHITE" and "BROWN".

--- src/components/email-templates/email-confirmation.js
+++ src/components/email-templates/email-confirmation.js
@@ -74,7 +74,15 @@ export function fullPaymentConfirmationTemplate(orderPayload) {
     specialRequests.push(notes.trim());
   }
   if (order.riceOption) {
-    specialRequests.push(`Rice Option: ${order.riceOption}`);
+    if (order.riceOption === 'WHITE'){
+      specialRequests.push(`All White Rice`);
+    }
+    else if (order.riceOption === 'BROWN'){
+      specialRequests.push(`All Brown Rice`);
+    }
+    else{
+      specialRequests.push(`No Rice Preference`);
+    }
   }
 
   // Build delivery address
